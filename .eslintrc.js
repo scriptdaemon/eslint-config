8010c803d11121ab55a907f6872111edb7dcb974
Kenneth Williams <scriptdaemon@users.noreply.github.com>
2017-12-14 23:32:06 -0800

    chore(deps): Update `eslint-plugin-jest` to 21.5.0
    This update automatically includes the 'jest' plugin and 'jest/globals'
    environment, so we no longer need to manually include them.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
-  plugins: [
-    'jest'
-  ],
   extends: [
     'eslint:recommended',
     'plugin:jest/recommended',
     'plugin:vue/recommended',
     'standard'
-  ],
-  env: {
-    'jest/globals': true
-  }
+  ]
 }
